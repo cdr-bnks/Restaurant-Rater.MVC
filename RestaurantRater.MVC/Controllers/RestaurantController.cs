dd0455e4c212db89c192d3417012bd6004d27c07
Garry Banks <ellisbanks24@gmail.com>
2022-01-12 12:52:46 -0500

    Restaurant/Create established

--- RestaurantRater.MVC/Controllers/RestaurantController.cs
+++ RestaurantRater.MVC/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantRater.MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,33 @@ namespace RestaurantRater.MVC.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestaurantDbContext _context = new RestaurantDbContext();
         // GET: Restaurant/Index
         public ActionResult Index()
         {
+            return View(_context.Restaurants.ToList());
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        //POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Restaurants.Add(restaurant);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(restaurant);
+            }
         }
     }
 }
