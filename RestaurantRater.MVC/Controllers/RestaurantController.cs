23edfe7efab812ab1429ed27063f092a68db200d
Garry Banks <ellisbanks24@gmail.com>
2022-01-13 19:42:14 -0500

    Restaurant/Edit Implemented

--- RestaurantRater.MVC/Controllers/RestaurantController.cs
+++ RestaurantRater.MVC/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRater.MVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -65,6 +66,48 @@ namespace RestaurantRater.MVC.Controllers
             _context.SaveChanges();
             // Just try Redirect next time
             return RedirectToAction("Index");
+        }
+        //GET: Restaurant/Edit/{id}
+        
+        public ActionResult Edit(int? id)
+        { // Get an id from the user
+
+            if (id == null)
+            { // Handle if the id is null
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            // Find Restaurant by that id
+            Restaurant restaurant = _context.Restaurants.Find(id);
+
+            if (restaurant == null)
+            {// If the Restaurant doesn't exist
+                return HttpNotFound();
+            }
+            else
+            { // Return the restaurant and the view
+                return View(restaurant);
+            }
+        }
+
+        //POST: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        { // Get restaurant
+
+            // If restaurant has any errors handle that
+            if (ModelState.IsValid)
+            {// Find the Entry(Method) model in the DB, access its State(Prop) and let user know it is modified
+                _context.Entry(restaurant).State = EntityState.Modified;
+                // Find any row that has been modified and update it
+                _context.SaveChanges();
+                // Once this action is completed return to the Index
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(restaurant);
+            }
         }
     }
 }
